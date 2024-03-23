e8e16d3e0051e1e2bb0c4a15e7f9dbc9ee50c69f
Shishir-rahman <sarkershishir4@gmail.com>
2024-03-23 22:09:39 +0600

    t

--- javascript.js
+++ javascript.js
@@ -1,29 +1,10 @@
 
 document.getElementById("demo").innerHTML = "Shishir";
 
-/*
-let a = parseInt(prompt("Enter the first Value"));
-    b = parseInt(prompt("Enter the last value"));
-    i=a;
-    sum = 0;
 
-while(i<=b){
-    sum = sum + i;
-    i=i+1;
+let x= parseInt(prompt("Enter First value"));
+    y = parseInt(prompt("Enter Last value"));
+
+for (let a=x; a<=y; a++){
+    document.write(" " +a);
 }
-
-document.write(sum);
-*/ 
-
-let x = 1;
-    sum = 0;
-
-    while(x<=100){
-        
-        if(x%5==0){
-            sum = sum + x;
-        }
-
-        x = x + 1;
-    }
-document.write(sum);
