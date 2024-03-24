2e0756b00e285300ba2941a6cdce5d1000b22595
Shishir-rahman <157983717+Shishir-rahman@users.noreply.github.com>
2024-03-24 15:26:05 +0600

    t

--- javascript.js
+++ javascript.js
@@ -2,9 +2,57 @@
 document.getElementById("demo").innerHTML = "Shishir";
 
 
-let x= parseInt(prompt("Enter First value"));
-    y = parseInt(prompt("Enter Last value"));
+function addition(x,y){
+    let result = x + y;
+    document.write("Sum = " + result + "<br>")
 
-for (let a=x; a<=y; a++){
-    document.write(" " +a);
 }
+
+
+function subtraction(x,y){
+    let result = x - y;
+    document.write("Sub = " + result + "<br>")
+
+}
+
+function multiply(x,y){
+    let result = x * y;
+    document.write("Multiply = "+ result + "<br>")
+}
+
+function divided(x, y){
+    let result = x/y;
+    document.write("Divided = "+ result + "<br>")
+}
+function modulus(x,y){
+    let result = x%y;
+    document.write("Modulus = "+ result + "<br>")
+}
+
+
+
+addition(5,6);
+addition(15,8);
+addition(50,61);
+
+subtraction(5, 8);
+subtraction(15, 7);
+subtraction(31, 4);
+
+multiply(5, 6);
+multiply(8, 2);
+multiply(18, 5);
+
+divided(6,2);
+divided(10,3);
+divided(25,5);
+
+modulus(5,2);
+modulus(10,5);
+modulus(50,21);
+
+
+(function total_sum(x,y){
+    let result = x + y;
+    document.write("Total sum = "+ result + "<br>");
+})(10, 15);
